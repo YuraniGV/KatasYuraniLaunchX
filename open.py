b3e33398ac8d72f7373604720cee8499c1ea4970
YuraniGV <99155434+YuraniGV@users.noreply.github.com>
2022-02-14 14:41:35 -0600

    Katas del propedéutico del LaunchX
    Katas y archivos realizados durante el propedéutico del LaunchX de Innovaccion.

---
+++ open.py
@@ -0,0 +1,16 @@
+#def main():
+#    open("/path/to/mars.jpg")
+
+#if __name__ == '__main__':
+#    main()
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+
+
+main()
